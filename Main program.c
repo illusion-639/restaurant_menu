754290b0459b6dfeabcee195b1b3b087bdb001c5
illusion-639 <deluluisthebestsolulu06@gmail.com>
2025-10-27 10:58:00 +0530

    Update Main program.c

--- Main program.c
+++ Main program.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 int main()
 {
-  char choice[20]; //subject to change
-  printf("welcome to our cafe. Now,what are we craving at?");
-printf("1.starters/snacks");
-printf("2.main course-indian famous");
-printf("3.main course-punjabi tadka");
-printf("4.main course-south special");
-printf("5.main course-chinese/japanese taste-");
-
+  char choice[100]; //subject to change
+  printf("welcome to our cafe. Now,what are we craving at?\n");
+printf("1.starters/snacks\n");
+printf("2.main course-indian famous\n");
+printf("3.main course-punjabi tadka\n");
+printf("4.main course-south special\n");
+printf("5.main course-chinese/japanese taste-\n");
+printf("....");
   //keep on adding
-  scanf("%[^/n]s", &choice);
+  scanf("%[^/n]s",choice);
+  printf("%s",choice);
 //incomplete work for now
+return 0;
+}
